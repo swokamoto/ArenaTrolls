c6f0fe5fe525e32b201ac988c426e63e2738b08b
swokamoto <swokamoto@gmail.com>
2023-12-12 14:00:23 -0500

    troubleshooting OAuth 1

--- public/googleAuth.js
+++ public/googleAuth.js
@@ -17,18 +17,16 @@ var uiConfig = {
     signInSuccessUrl: '<url-to-redirect-to-on-success>',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
-    tosUrl: '<your-tos-url>',
+    tosUrl: 'https://arenatroll-91061.web.app/',
     // Privacy policy url.
     privacyPolicyUrl: '<your-privacy-policy-url>'
     };
 
 // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
+
+// Create a Google authentication provider instance
+var provider = new firebase.auth.GoogleAuthProvider();
