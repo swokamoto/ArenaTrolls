19f813d77a0e8784d2a5c66bdcdf610a2c98a5ed
swokamoto <swokamoto@gmail.com>
2023-12-12 19:28:03 -0500

    TS OAuth 21

---
+++ public/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCNsSgu75foPXFVGUqPveByREhL6ymf68I",
+  authDomain: "arenatroll-91061.firebaseapp.com",
+  projectId: "arenatroll-91061",
+  storageBucket: "arenatroll-91061.appspot.com",
+  messagingSenderId: "1088008893718",
+  appId: "1:1088008893718:web:19c4b3d57762345b884c37",
+  measurementId: "G-4EQDGZ3VWX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
