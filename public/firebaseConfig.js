b14c2e83821d2d99d21aadc3cf009c3000509d24
swokamoto <swokamoto@gmail.com>
2023-12-15 12:19:21 -0500

    TS OAuth 28

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
